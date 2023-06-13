2044fc92a7371fd5b08dd42ce0fdc8625cf71f22
songhappy <guoqiongsong@gmail.com>
2023-06-12 20:02:10 -0700

    bloom

--- lm_eval/models/__init__.py
+++ lm_eval/models/__init__.py
@@ -6,12 +6,15 @@ from . import dummy
 
 MODEL_REGISTRY = {
     "hf": gpt2.HFLM,
-    "hf-causal": huggingface.AutoCausalLM,
+    "hf-causal": gpt2.HFLM,
+    "hf-causal-experimental": huggingface.AutoCausalLM,
     "hf-seq2seq": huggingface.AutoSeq2SeqLM,
     "gpt2": gpt2.GPT2LM,
     "gpt3": gpt3.GPT3LM,
     "textsynth": textsynth.TextSynthLM,
     "dummy": dummy.DummyLM,
+    "llama-cpp": gpt2.LlamaCPPLM,
+    "bloomz-cpp": gpt2.BloomzCPPLM
 }
 
 
